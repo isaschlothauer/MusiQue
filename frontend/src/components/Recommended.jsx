bb858e6b166bc3f92a877140de7034b55d8ffc60
Elif Gömleksiz <e.goemlek@gmail.com>
2022-10-27 22:53:21 +0200

    renamed css module classnames

--- frontend/src/components/Recommended.jsx
+++ frontend/src/components/Recommended.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import RecommendedCSS from "./Recommended.module.css";
+import styles from "./Recommended.module.css";
 import "../App.css";
 import Profile from "../assets/Joao.jpg";
 
 function Recommended() {
   return (
-    <div className={RecommendedCSS.rec}>
-      <h2 className={`${RecommendedCSS.h2}`}>Picked for you with ❤</h2>
-      <div className={RecommendedCSS.recQuote}>
-        <div className={RecommendedCSS.imgContainer}>
-          <img className={RecommendedCSS.img} alt="Joao" src={Profile} />
-          <div className={RecommendedCSS.quotation}>“</div>
+    <div className={styles.rec}>
+      <h2 className={`${styles.h2}`}>Picked for you with ❤</h2>
+      <div className={styles.recQuote}>
+        <div className={styles.imgContainer}>
+          <img className={styles.img} alt="Joao" src={Profile} />
+          <div className={styles.quotation}>“</div>
         </div>
-        <p className={`${RecommendedCSS.pText} ${"pText"}`}>
+        <p className={`${styles.pText} ${"pText"}`}>
           As soon the lyrics start, there is a mention of an individual that
           isn’t “here”, but was greatly dear, which is most likely Syd Barrett.
           Not only they miss him, but also the time when they were close. Bound
@@ -20,12 +20,12 @@ function Recommended() {
           nostalgia, but learn with our past. Sometimes, things can’t be
           avoided, but we can surely learn from them.{" "}
         </p>
-        <p className={`${RecommendedCSS.pTextName} ${"pText"}`}>
+        <p className={`${styles.pTextName} ${"pText"}`}>
           Joao, Germany/Portugal
         </p>
       </div>
 
-      <button type="button" className={RecommendedCSS.btn}>
+      <button type="button" className={styles.btn}>
         Spotify Logo
       </button>
     </div>
