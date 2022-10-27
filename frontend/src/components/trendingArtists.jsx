c7a5d1e16b1c369c0cb426ec95fe3008b67cee39
Kukuruzka1 <irinakukuruza295@gmail.com>
2022-10-27 22:08:27 +0200

    added desktop version for trending artists

--- frontend/src/components/trendingArtists.jsx
+++ frontend/src/components/trendingArtists.jsx
@@ -1,36 +1,45 @@
 import React from "react";
-import "./trendingArtists.css";
+import TrendingArtCSS from "./trendingArtists.module.css";
 
 function TrendingArtists() {
   return (
-    <div className="trending-artists">
+    <div className={TrendingArtCSS.trendingArtists}>
       <h2>Trending Artists</h2>
-      <div className="artists-card">
-        <article>
+      <div className={TrendingArtCSS.artistsCard}>
+        <article className={TrendingArtCSS.articles}>
           <img
             src="https://images.unsplash.com/photo-1619983081593-e2ba5b543168?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
             alt="album"
           />
-          <p className="p-title">THEA WANG, OTHER ARTIST</p>
-          <p className="p-italic">Tell me about it</p>
+          <div className={TrendingArtCSS.albumText}>
+            <p className="pTitle">THEA WANG, OTHER ARTIST</p>
+            <p className="pItalic">Tell me about it</p>
+          </div>
         </article>
-        <article>
+        <article className={TrendingArtCSS.articles}>
           <img
             src="https://images.unsplash.com/photo-1619983081593-e2ba5b543168?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
             alt="album"
           />
-          <p className="p-title">THEA WANG, OTHER ARTIST</p>
-          <p className="p-italic">Tell me about it</p>
+          <div className={TrendingArtCSS.albumText}>
+            <p className="pTitle">THEA WANG, OTHER ARTIST</p>
+            <p className="pItalic">Tell me about it</p>
+          </div>
         </article>
-        <article>
+        <article className={TrendingArtCSS.articles}>
           <img
             src="https://images.unsplash.com/photo-1619983081593-e2ba5b543168?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
             alt="album"
           />
-          <p className="p-title">THEA WANG, OTHER ARTIST</p>
-          <p className="p-italic">Tell me about it</p>
+          <div className={TrendingArtCSS.albumText}>
+            <p className="pTitle">THEA WANG, OTHER ARTIST</p>
+            <p className="pItalic">Tell me about it</p>
+          </div>
         </article>
-        <button type="button" className="p-buttons">
+        <button
+          type="button"
+          className={`${TrendingArtCSS.pButtons} ${"pButtons"}`}
+        >
           SHUFFLE
         </button>
       </div>
