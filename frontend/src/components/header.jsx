5d48d3b91fd6f7ea9f2e0b6d9ce0b84f5baf73ef
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2022-10-30 11:20:56 +0000

    Correction Header.jsx

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -4,16 +4,18 @@ import HeaderM from "./Header.module.css";
 function Header() {
   return (
     <div className={HeaderM.header}>
-      <img src="src/assets/musiqueLogo.png" alt="logo" />
-      <h2 className={HeaderM.headerFirsttext}>
+      <img
+        className={HeaderM.image2}
+        src="src/assets/musiqueLogo.png"
+        alt="logo"
+      />
+      <h1 className={`${HeaderM.headerFirsttext} ${"h1"}`}>
         musiQue presents you a new opportunity to explore the world of music.
-      </h2>
-      <p className={HeaderM.headerSecondtext}>
+      </h1>
+      <p className={`${HeaderM.headerSecondtext} ${"pItalic"}`}>
         Find out more about your favorite genres, discover new songs, and check
         the recommendations by our team of curators!
       </p>
     </div>
   );
 }
-
-export default Header;
