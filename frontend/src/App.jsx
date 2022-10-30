4dc07cf25d5993d8c99e5e1af46ceda37560f86a
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2022-10-30 12:14:25 +0000

    Erased Old Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/header";
-import Footer from "./components/footer";
 import GenreSection from "./components/genre";
 
 function App() {
   return (
     <div>
-      <Header />
       <GenreSection />
-      <Footer />
     </div>
   );
 }
