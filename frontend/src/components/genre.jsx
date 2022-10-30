521f2309431053204e0340dd4c257d6c04a9c73c
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2022-10-30 12:35:00 +0000

    Correction genre.jsx

--- frontend/src/components/genre.jsx
+++ frontend/src/components/genre.jsx
@@ -4,10 +4,84 @@ import Genre from "./Genre.module.css";
 function GenreSection() {
   return (
     <div className={Genre.genre}>
-      <h2 className={Genre.genretext}> Explore Genres</h2>
-      <button className={Genre.genrebutton} type="button">
-        <span className={Genre.genrespan}>ROCK</span>
-      </button>
+      <h2 className={Genre.title}> Discover Genres</h2>
+      <div className={Genre.buttonsdesk1}>
+        <button className={`${Genre.btn} ${"pButtons"}`} type="button">
+          <span>CLASSIC</span>
+        </button>
+        <button className={`${Genre.btn} ${"pButtons"}`} type="button">
+          <span>COUNTRY</span>
+        </button>
+        <button className={`${Genre.btn} ${"pButtons"}`} type="button">
+          <span>JAZZ</span>
+        </button>
+        <button className={`${Genre.btn} ${"pButtons"}`} type="button">
+          <span>METAL</span>
+        </button>
+        <button className={`${Genre.btn} ${"pButtons"}`} type="button">
+          <span>INDIE</span>
+        </button>
+        <button className={`${Genre.btn} ${"pButtons"}`} type="button">
+          <span>HIP-HOP</span>
+        </button>
+        <button className={`${Genre.btn} ${"pButtons"}`} type="button">
+          <span>REGGAE</span>
+        </button>
+        <button className={`${Genre.btn} ${"pButtons"}`} type="button">
+          <span>ROCK</span>
+        </button>
+        <button className={`${Genre.btn} ${"pButtons"}`} type="button">
+          <span>R&B</span>
+        </button>
+        <button className={`${Genre.btn} ${"pButtons"}`} type="button">
+          <span>PUNK</span>
+        </button>
+        <button className={`${Genre.btn} ${"pButtons"}`} type="button">
+          <span>ELECTRO</span>
+        </button>
+        <button className={`${Genre.btn} ${"pButtons"}`} type="button">
+          <span>BLUES</span>
+        </button>
+      </div>
+
+      <div className={Genre.buttonsdesk}>
+        <button className={`${Genre.btn2} ${"pButtons"}`} type="button">
+          ROCK
+        </button>
+        <button className={`${Genre.btn2} ${"pButtons"}`} type="button">
+          R&B
+        </button>
+        <button className={`${Genre.btn2} ${"pButtons"}`} type="button">
+          JAZZ
+        </button>
+        <button className={`${Genre.btn2} ${"pButtons"}`} type="button">
+          METAL
+        </button>
+        <button className={`${Genre.btn2} ${"pButtons"}`} type="button">
+          INDIE
+        </button>
+        <button className={`${Genre.btn2} ${"pButtons"}`} type="button">
+          HIP-HOP
+        </button>
+        <button className={`${Genre.btn2} ${"pButtons"}`} type="button">
+          REGGAE
+        </button>
+        <button className={`${Genre.btn2} ${"pButtons"}`} type="button">
+          CLASSIC
+        </button>
+        <button className={`${Genre.btn2} ${"pButtons"}`} type="button">
+          ELECTRO
+        </button>
+        <button className={`${Genre.btn2} ${"pButtons"}`} type="button">
+          COUNTRY
+        </button>
+        <button className={`${Genre.btn2} ${"pButtons"}`} type="button">
+          PUNK
+        </button>
+        <button className={`${Genre.btn2} ${"pButtons"}`} type="button">
+          BLUES
+        </button>
+      </div>
     </div>
   );
 }
