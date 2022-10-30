40b01bc6e0400590c60553bc1c31d90709f02692
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2022-10-30 12:05:09 +0000

    Correction aboutus.jsx

--- frontend/src/components/aboutus.jsx
+++ frontend/src/components/aboutus.jsx
@@ -4,18 +4,28 @@ import Aboutus from "./Aboutus.module.css";
 function AboutUs() {
   return (
     <div className={Aboutus.aboutus}>
-      <h2 className={Aboutus.aboutustitle}> About Us</h2>
-      <p className={Aboutus.aboutustext}>
-        Life is full of mysterious delights, but the one that we can’t live
-        without is music. It accompanies us every day, an art that’s
-        intrinsically connected with who we are. Discovering music is a cheer
-        pleasure, one that our musiQue team is passionate to share with you! We
-        aim to provide our visitors with a useful mean to discover the musical
-        world, such as accessing top charts, discovering new songs and new
-        artists, as well as providing an environment to curate music in all its
-        amazement!
-      </p>
-      <img src="src/assets/musiqueLogo.png" alt="logo" />
+      <div className={Aboutus.container}>
+        <h2 className={`${Aboutus.aboutustitle} ${"h2"}`}> About Us</h2>
+        <p className={`${Aboutus.aboutustext} ${"pText"}`}>
+          Life is full of mysterious delights, but the one that we can’t live
+          without is music. It accompanies us every day, an art that’s
+          intrinsically connected with who we are. Discovering music is a cheer
+          pleasure, one that our musiQue team is passionate to share with you!
+          We aim to provide our visitors with a useful mean to discover the
+          musical world, such as accessing top charts, discovering new songs and
+          new artists, as well as providing an environment to curate music in
+          all its amazement!
+        </p>
+      </div>
+      <div className={Aboutus.container2}>
+        <img
+          className={Aboutus.logo}
+          src="src/assets/musiqueLogo.png"
+          alt="logo"
+          width="50"
+          height="50"
+        />
+      </div>
     </div>
   );
 }
