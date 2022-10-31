66232f75cb7c57df7846598bf845c782acdce2d9
JoaoMota <joaoefmota@gmail.com>
2022-10-31 15:30:20 +0100

    added spotify logo component to make Genre page work as they are dependent of one another

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,32 +6,12 @@ import { Route, Routes } from "react-router-dom";
 // import Footer from "./components/footer";
 // import GenreSection from "./components/genre";
 
-import Header from "./components/header";
-import GenreSection from "./components/GenreSection";
-import Recommended from "./components/Recommended";
-import TrendingArtists from "./components/trendingArtists";
-import AboutUs from "./components/AboutUs";
-import Footer from "./components/footer";
-
 import Rock from "./pages/Rock";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <Header />
-              <GenreSection />
-              <TrendingArtists />
-              <Recommended />
-              <AboutUs />
-              <Footer />
-            </div>
-          }
-        />
         <Route path="/rock/*" element={<Rock />} />
       </Routes>
     </div>
