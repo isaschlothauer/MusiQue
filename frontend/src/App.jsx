6e55d11b1ae53116f5c73331ec5c065653c346e8
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2022-11-02 15:52:12 +0000

    Updated App.jsx (footer)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,24 +6,29 @@ import Recommended from "./components/Recommended";
 import WorldMusic from "./components/WorldMusic";
 import Header from "./components/header";
 import GenreSection from "./components/genre";
-/* import Footer from "./components/footer"; */
+import Footer from "./components/footer";
 
 import Rock from "./pages/Rock";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route>
-          <Header />
-          <GenreSection />
-          <Route path="/rock/*" element={<Rock />} />
-          <Recommended />
-          <WorldMusic />
-        </Route>
-      </Routes>
-    </div>
-  );
+return (
+<div className="App">
+<Routes>
+<Route
+path="/"
+element={
+<div>
+<Header />
+<Recommended />
+<WorldMusic />
+<Footer />
+</div>
+}
+/>
+<Route path="/rock/*" element={<Rock />} />
+</Routes>
+</div>
+);
 }
 
 export default App;
