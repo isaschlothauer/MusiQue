e723e724a979cd212ca6540a0c729c2fd7d2fdf9
Kukuruzka1 <irinakukuruza295@gmail.com>
2022-11-04 16:55:47 +0100

    added spotify logo for desktop version and adjusted destop and mobile version

--- frontend/src/components/trendingArtists.jsx
+++ frontend/src/components/trendingArtists.jsx
@@ -1,25 +1,30 @@
+import React from "react";
 import TrendingArtCSS from "./trendingArtists.module.css";
+import "../App.css";
 import ShuffleButton from "./shuffleButton";
 import SpotifyLogoButton from "./SpotifyLogoButton";
 
 function TrendingArtists() {
   return (
     <div className={TrendingArtCSS.trendingArtists}>
+      <h2 className={TrendingArtCSS.sectionTitle}>Trending Artists</h2>
       <div className={TrendingArtCSS.artistsCard}>
-        <h2>Trending Artists</h2>
         <article className={TrendingArtCSS.articles}>
           <div className={TrendingArtCSS.albumAndTextContainer}>
             <div className={TrendingArtCSS.coversongContainer}>
               <img
-                id={TrendingArtCSS.img}
                 src="https://images.unsplash.com/photo-1619983081593-e2ba5b543168?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                 alt="album"
               />
               <div className={TrendingArtCSS.playButton} />
             </div>
             <div className={TrendingArtCSS.albumText}>
-              <p className="pTitle">THEA WANG, OTHER ARTIST</p>
-              <p className="pItalic">Tell me about it</p>
+              <p className={`${TrendingArtCSS.songTitle} ${"pTitle"}`}>
+                THEA WANG, OTHER ARTIST
+              </p>
+              <p className={`${TrendingArtCSS.songName} ${"pItalic"}`}>
+                Tell me about it
+              </p>
             </div>
           </div>
           <img
@@ -28,7 +33,13 @@ function TrendingArtists() {
             className={TrendingArtCSS.spotifyIconLittle}
             width={28}
           />
-          <SpotifyLogoButton />
+          <img
+            src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_Black.png"
+            alt="spotify-icon"
+            className={TrendingArtCSS.spotifyIconLittleBlack}
+            width={28}
+          />
+          <SpotifyLogoButton className={TrendingArtCSS.SpotifyIconBig} />
         </article>
         <article className={TrendingArtCSS.articles}>
           <div className={TrendingArtCSS.albumAndTextContainer}>
@@ -40,8 +51,12 @@ function TrendingArtists() {
               <div className={TrendingArtCSS.playButton} />
             </div>
             <div className={TrendingArtCSS.albumText}>
-              <p className="pTitle">THEA WANG, OTHER ARTIST</p>
-              <p className="pItalic">Tell me about it</p>
+              <p className={`${TrendingArtCSS.songTitle} ${"pTitle"}`}>
+                THEA WANG, OTHER ARTIST
+              </p>
+              <p className={`${TrendingArtCSS.songName} ${"pItalic"}`}>
+                Tell me about it
+              </p>
             </div>
           </div>
           <img
@@ -50,7 +65,13 @@ function TrendingArtists() {
             className={TrendingArtCSS.spotifyIconLittle}
             width={28}
           />
-          <SpotifyLogoButton />
+          <img
+            src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_Black.png"
+            alt="spotify-icon"
+            className={TrendingArtCSS.spotifyIconLittleBlack}
+            width={28}
+          />
+          <SpotifyLogoButton className={TrendingArtCSS.SpotifyIconBig} />
         </article>
         <article className={TrendingArtCSS.articles}>
           <div className={TrendingArtCSS.albumAndTextContainer}>
@@ -62,8 +83,12 @@ function TrendingArtists() {
               <div className={TrendingArtCSS.playButton} />
             </div>
             <div className={TrendingArtCSS.albumText}>
-              <p className="pTitle">THEA WANG, OTHER ARTIST</p>
-              <p className="pItalic">Tell me about it</p>
+              <p className={`${TrendingArtCSS.songTitle} ${"pTitle"}`}>
+                THEA WANG, OTHER ARTIST
+              </p>
+              <p className={`${TrendingArtCSS.songName} ${"pItalic"}`}>
+                Tell me about it
+              </p>
             </div>
           </div>
           <img
@@ -72,7 +97,13 @@ function TrendingArtists() {
             className={TrendingArtCSS.spotifyIconLittle}
             width={28}
           />
-          <SpotifyLogoButton />
+          <img
+            src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_Black.png"
+            alt="spotify-icon"
+            className={TrendingArtCSS.spotifyIconLittleBlack}
+            width={28}
+          />
+          <SpotifyLogoButton className={TrendingArtCSS.SpotifyIconBig} />
         </article>
         <ShuffleButton />
       </div>
