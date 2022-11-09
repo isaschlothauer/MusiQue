368a959e0b02bcfac3633139d2a866dbc1d645f2
Kukuruzka1 <irinakukuruza295@gmail.com>
2022-11-09 16:09:26 +0100

    added functionality to shuffle button

--- frontend/src/components/trendingArtists.jsx
+++ frontend/src/components/trendingArtists.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import TrendingArtCSS from "./trendingArtists.module.css";
 import "../App.css";
-import ShuffleButton from "./shuffleButton";
 import SpotifyLogoButton from "./SpotifyLogoButton";
 
 const CLIENT_ID = "d6b767f2085441d5bd7a2c4b59b009a6";
@@ -16,7 +15,10 @@ const CLIENT_SECRET = "3db89dc2644044a3baa93a83ca6f7f6c";
 
 function TrendingArtists() {
   const [token, setToken] = useState("");
-  const [artistSong, setArtistSong] = useState("");
+  const [artistSong1, setArtistSong1] = useState("");
+  const [artistSong2, setArtistSong2] = useState("");
+  const [artistSong3, setArtistSong3] = useState("");
+  const [renderCheck, setRenderCheck] = useState(false);
 
   useEffect(() => {
     // API access token
@@ -36,126 +38,150 @@ function TrendingArtists() {
       });
   }, []);
 
-  useEffect(() => {
-    if (token == null) return;
-    fetch("https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((result) => setArtistSong(result));
-  }, [token]);
+  async function generateSong() {
+    useEffect(() => {
+      if (token == null) return;
+      fetch("https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          const n = Math.floor(Math.random() * 50);
+          const song1 = result.tracks.items[n].track;
+          const song2 = result.tracks.items[n + 1].track;
+          const song3 = result.tracks.items[n + 2].track;
+
+          const album1 = {
+            album1Image: song1.album.images[1].url,
+            album1ArtistName: song1.artists[0].name,
+            album1SongName: song1.name,
+          };
+
+          const album2 = {
+            album2Image: song2.album.images[1].url,
+            album2ArtistName: song2.artists[0].name,
+            album2SongName: song2.name,
+          };
+
+          const album3 = {
+            album3Image: song3.album.images[1].url,
+            album3ArtistName: song3.artists[0].name,
+            album3SongName: song3.name,
+          };
+
+          setArtistSong1(album1);
+          setArtistSong2(album2);
+          setArtistSong3(album3);
+        });
+    }, [renderCheck, token]);
+  }
+  generateSong();
 
   return (
     <div className={TrendingArtCSS.trendingArtists}>
-      {artistSong.tracks != null && artistSong.tracks.items != null ? (
-        <>
-          <h2 className={TrendingArtCSS.sectionTitle}>Trending Artists</h2>
-          <div className={TrendingArtCSS.artistsCard}>
-            <article className={TrendingArtCSS.articles}>
-              <div className={TrendingArtCSS.albumAndTextContainer}>
-                <div className={TrendingArtCSS.coversongContainer}>
-                  <img
-                    src={artistSong.tracks.items[0].track.album.images[1].url}
-                    alt="album"
-                  />
-                  <div className={TrendingArtCSS.playButton} />
-                </div>
-                <div className={TrendingArtCSS.albumText}>
-                  <p className={`${TrendingArtCSS.songTitle} ${"pTitle"}`}>
-                    {artistSong.tracks.items[0].track.artists[0].name}
-                  </p>
-                  <p className={`${TrendingArtCSS.songName} ${"pItalic"}`}>
-                    {artistSong.tracks.items[0].track.name}
-                  </p>
-                </div>
-              </div>
-              <img
-                src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_White.png"
-                alt="spotify-icon"
-                className={TrendingArtCSS.spotifyIconLittle}
-                width={28}
-              />
-              <img
-                src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_Black.png"
-                alt="spotify-icon"
-                className={TrendingArtCSS.spotifyIconLittleBlack}
-                width={28}
-              />
-              <SpotifyLogoButton className={TrendingArtCSS.SpotifyIconBig} />
-            </article>
-            <article className={TrendingArtCSS.articles}>
-              <div className={TrendingArtCSS.albumAndTextContainer}>
-                <div className={TrendingArtCSS.coversongContainer}>
-                  <img
-                    src={artistSong.tracks.items[1].track.album.images[1].url}
-                    alt="album"
-                  />
-                  <div className={TrendingArtCSS.playButton} />
-                </div>
-                <div className={TrendingArtCSS.albumText}>
-                  <p className={`${TrendingArtCSS.songTitle} ${"pTitle"}`}>
-                    {artistSong.tracks.items[1].track.artists[0].name}
-                  </p>
-                  <p className={`${TrendingArtCSS.songName} ${"pItalic"}`}>
-                    {artistSong.tracks.items[1].track.name}
-                  </p>
-                </div>
-              </div>
-              <img
-                src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_White.png"
-                alt="spotify-icon"
-                className={TrendingArtCSS.spotifyIconLittle}
-                width={28}
-              />
-              <img
-                src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_Black.png"
-                alt="spotify-icon"
-                className={TrendingArtCSS.spotifyIconLittleBlack}
-                width={28}
-              />
-              <SpotifyLogoButton className={TrendingArtCSS.SpotifyIconBig} />
-            </article>
-            <article className={TrendingArtCSS.articles}>
-              <div className={TrendingArtCSS.albumAndTextContainer}>
-                <div className={TrendingArtCSS.coversongContainer}>
-                  <img
-                    src={artistSong.tracks.items[2].track.album.images[1].url}
-                    alt="album"
-                  />
-                  <div className={TrendingArtCSS.playButton} />
-                </div>
-                <div className={TrendingArtCSS.albumText}>
-                  <p className={`${TrendingArtCSS.songTitle} ${"pTitle"}`}>
-                    {artistSong.tracks.items[2].track.artists[0].name}
-                  </p>
-                  <p className={`${TrendingArtCSS.songName} ${"pItalic"}`}>
-                    {artistSong.tracks.items[0].track.name}
-                  </p>
-                </div>
-              </div>
-              <img
-                src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_White.png"
-                alt="spotify-icon"
-                className={TrendingArtCSS.spotifyIconLittle}
-                width={28}
-              />
-              <img
-                src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_Black.png"
-                alt="spotify-icon"
-                className={TrendingArtCSS.spotifyIconLittleBlack}
-                width={28}
-              />
-              <SpotifyLogoButton className={TrendingArtCSS.SpotifyIconBig} />
-            </article>
-            <ShuffleButton />
+      <h2 className={TrendingArtCSS.sectionTitle}>Trending Artists</h2>
+      <div className={TrendingArtCSS.artistsCard}>
+        <article className={TrendingArtCSS.articles}>
+          <div className={TrendingArtCSS.albumAndTextContainer}>
+            <div className={TrendingArtCSS.coversongContainer}>
+              <img src={artistSong1.album1Image} alt="album" />
+              <div className={TrendingArtCSS.playButton} />
+            </div>
+            <div className={TrendingArtCSS.albumText}>
+              <p className={`${TrendingArtCSS.songTitle} ${"pTitle"}`}>
+                {artistSong1.album1ArtistName}
+              </p>
+              <p className={`${TrendingArtCSS.songName} ${"pItalic"}`}>
+                {artistSong1.album1SongName}
+              </p>
+            </div>
           </div>
-        </>
-      ) : (
-        <h2>Tracks loading...</h2>
-      )}
+          <img
+            src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_White.png"
+            alt="spotify-icon"
+            className={TrendingArtCSS.spotifyIconLittle}
+            width={28}
+          />
+          <img
+            src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_Black.png"
+            alt="spotify-icon"
+            className={TrendingArtCSS.spotifyIconLittleBlack}
+            width={28}
+          />
+          <SpotifyLogoButton className={TrendingArtCSS.SpotifyIconBig} />
+        </article>
+        <article className={TrendingArtCSS.articles}>
+          <div className={TrendingArtCSS.albumAndTextContainer}>
+            <div className={TrendingArtCSS.coversongContainer}>
+              <img src={artistSong2.album2Image} alt="album" />
+              <div className={TrendingArtCSS.playButton} />
+            </div>
+            <div className={TrendingArtCSS.albumText}>
+              <p className={`${TrendingArtCSS.songTitle} ${"pTitle"}`}>
+                {artistSong2.album2ArtistName}
+              </p>
+              <p className={`${TrendingArtCSS.songName} ${"pItalic"}`}>
+                {artistSong2.album2SongName}
+              </p>
+            </div>
+          </div>
+          <img
+            src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_White.png"
+            alt="spotify-icon"
+            className={TrendingArtCSS.spotifyIconLittle}
+            width={28}
+          />
+          <img
+            src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_Black.png"
+            alt="spotify-icon"
+            className={TrendingArtCSS.spotifyIconLittleBlack}
+            width={28}
+          />
+          <SpotifyLogoButton className={TrendingArtCSS.SpotifyIconBig} />
+        </article>
+        <article className={TrendingArtCSS.articles}>
+          <div className={TrendingArtCSS.albumAndTextContainer}>
+            <div className={TrendingArtCSS.coversongContainer}>
+              <img src={artistSong3.album3Image} alt="album" />
+              <div className={TrendingArtCSS.playButton} />
+            </div>
+            <div className={TrendingArtCSS.albumText}>
+              <p className={`${TrendingArtCSS.songTitle} ${"pTitle"}`}>
+                {artistSong3.album3ArtistName}
+              </p>
+              <p className={`${TrendingArtCSS.songName} ${"pItalic"}`}>
+                {artistSong3.album3SongName}
+              </p>
+            </div>
+          </div>
+          <img
+            src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_White.png"
+            alt="spotify-icon"
+            className={TrendingArtCSS.spotifyIconLittle}
+            width={28}
+          />
+          <img
+            src="src/assets/spotify-icons-logos/icons/01_RGB/02_PNG/Spotify_Icon_RGB_Black.png"
+            alt="spotify-icon"
+            className={TrendingArtCSS.spotifyIconLittleBlack}
+            width={28}
+          />
+          <SpotifyLogoButton className={TrendingArtCSS.SpotifyIconBig} />
+        </article>
+        <button
+          onClick={() => {
+            // eslint-disable-next-line no-unused-expressions
+            renderCheck !== true ? setRenderCheck(true) : setRenderCheck(false);
+          }}
+          type="button"
+          className={`${TrendingArtCSS.renderButton} ${"pButtons"}`}
+        >
+          SHUFFLE
+        </button>
+      </div>
     </div>
   );
 }
