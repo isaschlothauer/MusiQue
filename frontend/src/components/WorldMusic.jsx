d8aa4bbebeadc4327b3551d148804edf65552111
Isamu Schlothauer <isamu.schlothauer@gmail.com>
2022-11-10 09:06:48 +0100

    Fetch and randomization made independent. Before each fetch, playList is also randomly picked

--- frontend/src/components/WorldMusic.jsx
+++ frontend/src/components/WorldMusic.jsx
@@ -12,17 +12,14 @@ const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 // const artistName = "The Beatles";
 
-console.log(clientId);
-console.log(clientSecret);
-
-// console.log(login);
-
 // TO DO LIST
 // 1. Randomized artist selector
 // 3. Play List and song picker still needs to be implemented
 // 5. Implement error handing on null or undefined response
 
 // Find a way to get song tracks
+
+// Curated playlist pool
 const playLists = [
   {
     playListId: "37i9dQZF1DX0FGW2dUyDef",
@@ -36,25 +33,28 @@ const playLists = [
     playListId: "72U9wNe1fkxYW68gh0TbO3",
     country: "Iceland",
   },
+  {
+    playListId: "1ggnbwPgliav7xvkTQbRy8",
+    country: "Georgia",
+  },
+  {
+    playListId: "0cyH0TxFDgCQ2gQbs6shh2",
+    country: "Japan",
+  },
+  {
+    playListId: "37i9dQZF1DZ06evO4nuxuV",
+    country: "Iceland",
+  },
 ];
 
-function randomizer(num) {
-  const playListData = playLists[Math.floor(Math.random() * num)].playListId;
-  return playListData;
+// Multi purpose random nunerator
+function numGen(num) {
+  const result = Math.floor(Math.random() * num);
+  return result;
 }
-
-// Test random num gen
-function randNumGen(num) {
-  const randomNum = Math.floor(Math.random() * num);
-  return randomNum;
-}
-
-console.log(randNumGen(3));
 
 function WorldMusic() {
   // eslint-disable-next-line no-unused-vars
-  const [count, setCount] = useState(0);
-  const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [musicData1, setMusicData1] = useState("");
   const [musicData2, setMusicData2] = useState("");
@@ -73,11 +73,15 @@ function WorldMusic() {
       body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
     })
       .then((result) => result.json())
-      .then((data) => setAccessToken(data.access_token));
+      .then((data) => setAccessToken(data.access_token))
+      .catch((err) => {
+        console.error(err);
+      });
   }, []);
 
   async function generateArtistData() {
     useEffect(() => {
+      if (accessToken === null) return;
       // Authentication mechanism
       const artistParameters = {
         method: "GET",
@@ -87,19 +91,16 @@ function WorldMusic() {
         },
       };
 
-      // eslint-disable-next-line prefer-const
-      let currentPlayList = fetch(
-        `https://api.spotify.com/v1/playlists/${randomizer(3)}`,
+      const pListIndex1 = numGen(playLists.length);
+      const currentPlayList1 = fetch(
+        `https://api.spotify.com/v1/playlists/${playLists[pListIndex1].playListId}`,
         artistParameters
       )
         .then((res) => res.json())
         .then((data) => {
-          // console.log(data);
-          const n = Math.floor(Math.random() * 45);
-          const stringPath1 = data.tracks.items[n].track;
-          const stringPath2 = data.tracks.items[n + 1].track;
-          const stringPath3 = data.tracks.items[n + 2].track;
-          const stringPath4 = data.tracks.items[n + 3].track;
+          console.log(data);
+          const stringPath1 =
+            data.tracks.items[Math.floor(Math.random() * 50)].track;
 
           // Panel 1 data
           const panel1 = {
@@ -108,41 +109,70 @@ function WorldMusic() {
             panel1Album: stringPath1.album.name,
             panel1Release: stringPath1.album.release_date.split("-")[0],
             panel1Image: stringPath1.album.images[1].url,
-          };
-
-          // Panel 2 data
-          const panel2 = {
-            panel2Title: stringPath2.name,
-            panel2Artists: stringPath2.artists[0].name,
-            panel2Album: stringPath2.album.name,
-            panel2Release: stringPath2.album.release_date.split("-")[0],
-            panel2Image: stringPath2.album.images[1].url,
-          };
-
-          // Panel 3 data
-          const panel3 = {
-            panel3Title: stringPath3.name,
-            panel3Artists: stringPath3.artists[0].name,
-            panel3Album: stringPath3.album.name,
-            panel3Release: stringPath3.album.release_date.split("-")[0],
-            panel3Image: stringPath3.album.images[1].url,
-          };
-
-          // Panel 4 data
-          const panel4 = {
-            panel4Title: stringPath4.name,
-            panel4Artists: stringPath4.artists[0].name,
-            panel4Album: stringPath4.album.name,
-            panel4Release: stringPath4.album.release_date.split("-")[0],
-            panel4Image: stringPath4.album.images[1].url,
+            panel1Country: playLists[pListIndex1].country,
           };
 
           // Savimg panel objects to state
           setMusicData1(panel1);
-          setMusicData2(panel2);
-          setMusicData3(panel3);
-          setMusicData4(panel4);
+        })
+        .catch((err) => console.error(err));
+
+      currentPlayList1.then((test) => {
+        const pListIndex2 = numGen(playLists.length);
+        const currentPlayList2 = fetch(
+          `https://api.spotify.com/v1/playlists/${playLists[pListIndex2].playListId}`,
+          artistParameters
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            const stringPath2 =
+              data.tracks.items[Math.floor(Math.random() * 50)].track;
+
+            // Panel 1 data
+            const panel2 = {
+              panel2Title: stringPath2.name,
+              panel2Artists: stringPath2.artists[0].name,
+              panel2Album: stringPath2.album.name,
+              panel2Release: stringPath2.album.release_date.split("-")[0],
+              panel2Image: stringPath2.album.images[1].url,
+              panel2Country: playLists[pListIndex2].country,
+            };
+
+            // Savimg panel objects to state
+            setMusicData2(panel2);
+          })
+          .catch((err) => console.error(err));
+
+        currentPlayList2.then((test3) => {
+          const pListIndex3 = numGen(playLists.length);
+          const currentPlayList3 = fetch(
+            `https://api.spotify.com/v1/playlists/${playLists[pListIndex3].playListId}`,
+            artistParameters
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              const stringPath3 =
+                data.tracks.items[Math.floor(Math.random() * 50)].track;
+
+              // Panel 1 data
+              const panel3 = {
+                panel3Title: stringPath3.name,
+                panel3Artists: stringPath3.artists[0].name,
+                panel3Album: stringPath3.album.name,
+                panel3Release: stringPath3.album.release_date.split("-")[0],
+                panel3Image: stringPath3.album.images[1].url,
+                panel3Country: playLists[pListIndex3].country,
+              };
+              // Savimg panel objects to state
+              setMusicData3(panel3);
+            })
+            .catch((err) => console.error(err));
+
+          currentPlayList3.then((test4) => {
+            const pListIndex4 = numGen(playLists.length);
+          });
         });
+      });
     }, [renderCheck, accessToken]);
   }
 
@@ -165,7 +195,9 @@ function WorldMusic() {
             <p className={`${MusicCSS.artists} ${["pItalic"]}`}>
               {musicData1.panel1Artists}
             </p>
-            {/* <p className={`${MusicCSS.country} ${["pText"]}`}>Country: {musicData1.panel1Country}</p> */}
+            <p className={`${MusicCSS.country} ${["pText"]}`}>
+              Country: {musicData1.panel1Country}
+            </p>
             {/* <p className={`${MusicCSS.release} ${["pText"]}`}>
               Album: {musicData1.panel1Album}
             </p> */}
@@ -187,7 +219,9 @@ function WorldMusic() {
             <p className={`${MusicCSS.artists} ${["pItalic"]}`}>
               {musicData2.panel2Artists}
             </p>
-            {/* <p className={`${MusicCSS.country} ${["pText"]}`}>Country: {musicData1.panel1Country}</p> */}
+            <p className={`${MusicCSS.country} ${["pText"]}`}>
+              Country: {musicData2.panel2Country}
+            </p>
             {/* <p className={`${MusicCSS.release} ${["pText"]}`}>
               Album: {musicData2.panel2Album}
             </p> */}
@@ -209,7 +243,9 @@ function WorldMusic() {
             <p className={`${MusicCSS.artists} ${["pItalic"]}`}>
               {musicData3.panel3Artists}
             </p>
-            {/* <p className={`${MusicCSS.country} ${["pText"]}`}>Country: {musicData1.panel1Country}</p> */}
+            <p className={`${MusicCSS.country} ${["pText"]}`}>
+              Country: {musicData3.panel3Country}
+            </p>
             {/* <p className={`${MusicCSS.release} ${["pText"]}`}>
               Album: {musicData3.panel3Album}
             </p> */}
@@ -231,7 +267,9 @@ function WorldMusic() {
             <p className={`${MusicCSS.artists} ${["pItalic"]}`}>
               {musicData4.panel4Artists}
             </p>
-            {/* <p className={`${MusicCSS.country} ${["pText"]}`}>Country: {musicData1.panel1Country}</p> */}
+            <p className={`${MusicCSS.country} ${["pText"]}`}>
+              Country: {musicData4.panel4Country}
+            </p>
             {/* <p className={`${MusicCSS.release} ${["pText"]}`}>
               Album: {musicData4.panel4Album}
             </p> */}
