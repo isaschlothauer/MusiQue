27720d569c3359e5ec1e842de7346cb8825a23e2
Elif Gömleksiz <e.goemlek@gmail.com>
2022-11-15 10:52:07 +0100

    added new data & new api fetch with randomizer

--- frontend/src/components/Recommended.jsx
+++ frontend/src/components/Recommended.jsx
@@ -7,25 +7,79 @@ import Logo from "../assets/spotify_logo_white.png";
 
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
-const recs = {
-  author: "Joao, Germany/Portugal",
-  imgAuthor: { Profile },
-  content: `As soon the lyrics start, there is a mention of an individual that
-    isn’t “here”, but was greatly dear, which is most likely Syd
-    Barrett. Not only they miss him, but also the time when they were
-    close. Bound not by blood, but music. Life goes on: we shouldn’t
-    hold on for nostalgia, but learn with our past. Sometimes, things
-    can’t be avoided, but we can surely learn from them.`,
-  songID: "6mFkJmJqdDVQ1REhVfGgd1",
-  previewUrl:
-    "https://p.scdn.co/mp3-preview/e26e8dce7ef4bcb955ee0b1b4c8316f2daa3b942?cid=774b29d4f13844c495f206cafdad9c86",
-};
+const recs = [
+  {
+    author: "Joao, Germany/Portugal",
+    imgAuthor: { Profile },
+    content: `As soon the lyrics start, there is a mention of an individual that
+      isn’t “here”, but was greatly dear, which is most likely Syd
+      Barrett. Not only they miss him, but also the time when they were
+      close. Bound not by blood, but music. Life goes on: we shouldn’t
+      hold on for nostalgia, but learn with our past. Sometimes, things
+      can’t be avoided, but we can surely learn from them.`,
+    id: "6mFkJmJqdDVQ1REhVfGgd1",
+  },
+  {
+    author: "Isamu, Germany/Japan",
+    imgAuthor: { Profile },
+    content: `A song that takes you somewhere far away and unfamiliar, yet fills you with sense of nostalgia. Sound is airy and mystical, almost spiritual even. It is an aural delight and unforgettable emotional experience through sound. Whatever your preference in music may be, give yourself a chance to see where this music will take you.`,
+    id: "2HHAxkrJbuWZ7JWdxYbsWO",
+  },
+  {
+    author: "Elif, Germany",
+    imgAuthor: { Profile },
+    content: `Just by the rythm and singing, this song conveys a feeling of self-assurance and freedom for me - without understanding any of the words. It’s one of those songs, that lifts your mood up. It even feels cathartic (I mean, just listen to 2:19-3:10).`,
+    id: "3f8iimOXaVgOolPrAOrrFS",
+  },
+  {
+    author: "Iryna, Ukraine/Germany",
+    imgAuthor: { Profile },
+    content: `What do I love most about the song?... You can listen to it at any moment of your life and it will give you this nice feeling that
+    everything is going to be okay. I like the fact that lyrics can be about
+    your loved one, your parents, your friends, your kids, etc. Even
+    though the song came out in 1980 it’s still popular nowadays, as
+    many people say “Classic never dies”.`,
+    id: "1ko2lVN0vKGUl9zrU0qSlT",
+  },
+  {
+    author: "Iryna, Ukraine/Germany",
+    imgAuthor: { Profile },
+    content: `If someone tells you that Ukrainian rap is dead - feel free to play this song. It was released in 2021 by the famous Ukrainian band
+    “KALUSH” which won Eurovision in 2022. This is a story about the
+    daily struggle with our addictions, in which we are in the ring. Our
+    coach is the experience, and the audience is our environment.`,
+    id: "3ZVzwZevjBntQD8QDekVO1",
+  },
+  {
+    author: "Isamu, Germany/Japan",
+    imgAuthor: { Profile },
+    content: `I can't recommend this piece enough. This dazzling, yet poetically beautiful rendition by Gould has been among my most favorites for a long time. Carefully woven among his signature style, a glimpse of intimacy or poetic softness that can melt your heart. Simply wonderful performance.`,
+    id: "0sCl0tOYBfDJuMUHRiCeBZ",
+  },
+  {
+    author: "Elif, Germany",
+    imgAuthor: { Profile },
+    content: `"My branches scrape the sea of stars, My roots dig deep into this world of ours, I'm grounded”. I love the lyrics of this song. When I listen to it, I imagine myself dancing. My feet touching the ground to then again lift off - oscillating between imagination and reality. To not lose touch with the ground, nor with the sky. Can they exist at the same time?`,
+    id: "7cYod6eQvKmiyLwOjBgYR6",
+  },
+];
 
-// data.artists[0].name
-// data.id
-// data.name
-// data.preview_url
-// endpoint:  GET https://api.spotify.com/v1/tracks/{id}
+// NEW API WITH PLAYLIST TRACKS ENDPOINT
+// GET https://api.spotify.com/v1/playlists/{playlist_id}/tracks
+
+// data.items[i].track.artists[0].name => array with 1 or more artists
+// data.items[i].track.id   ===> track ID
+// data.items[i].track.name  ===> song title
+// data.items[i].track.preview_url
+// data.items[i].track.external_urls.spotify  ==> spotify Link
+
+// 1. fetch playlist tracks
+// 2. get a random track from API
+// const fetchedSongs = data.items;
+// const randomSong = fetchedSongs[Math.floor(Math.random() * fetchedSongs.length)]
+// 3. get the matching infos from recs array:
+// const matchedRec = recs.find((rec) => rec.id === randomSong.id);
+// const songData = [...matchedRec, ]
 
 function Recommended() {
   const [accessToken, setAccessToken] = useState("");
@@ -54,26 +108,37 @@ function Recommended() {
         Authorization: `Bearer ${accessToken}`,
       },
     };
-    fetch(`https://api.spotify.com/v1/tracks/${recs.songID}`, trackParameters)
+    fetch(
+      `https://api.spotify.com/v1/playlists/4B6LseBmS5BhXRYPvXIv6T/tracks`,
+      trackParameters
+    )
       .then((res) => res.json())
-      // eslint-disable-next-line no-restricted-syntax
       .then((data) => {
-        const fetchedSong = data;
+        const fetchedSongs = data.items;
+        // console.log(fetchedSongs);
+        const randomSong =
+          fetchedSongs[Math.floor(Math.random() * fetchedSongs.length)];
+        console.log(randomSong);
         const {
-          name: songTitle,
-          artists,
-          external_urls: { spotify: spotifyLink },
-        } = fetchedSong;
+          track: { id },
+          track: { name: songTitle },
+          track: { artists },
+          track: {
+            external_urls: { spotify: spotifyLink },
+          },
+          track: { preview_url: previewUrl },
+        } = randomSong;
         const artistTitle = artists.map((artist) => artist.name).join(", ");
+        const matchedRec = recs.find((rec) => rec.id === id);
         const songData = {
-          ...recs,
-          artistTitle,
+          ...matchedRec,
           songTitle,
+          artistTitle,
+          previewUrl,
           spotifyLink,
         };
-        setSong(songData); // eslint-disable-next-line no-restricted-syntax
-        console.log(data);
         console.log(songData);
+        setSong(songData);
       });
   }, [accessToken]);
 
