4ad6a1955d42554c1914c1201de21c1036f35e2d
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2023-03-17 16:05:58 +0000

    Tailwind Setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import "./index.css";
 import ScrollToTop from "./components/ScrollToTop";
 import App from "./App";
 
